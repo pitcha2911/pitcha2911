20b2d367d5db097d3672722e07de6a0ea201a6a8
pitcha2911 <pitcha2911@gmail.com>
2025-01-19 03:40:42 +0700

    Create assign1_5.py

---
+++ assign1_5.py
@@ -0,0 +1,18 @@
+import time
+import machine
+import onewire
+import ds18x20
+
+#DS18B20 data line connected to pin P10
+dat = machine.Pin(4)
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
+
+sensors = ds.scan()
+print('Found DS18B20 sensors:', sensors)
+
+ds.convert_temp()
+time.sleep_ms(750)
+schedule.every(3).seconds.do(sensor)
+while True:
+  for sensor in sensors:
+    print('Temperatre:', ds.read_temp(sensor))
